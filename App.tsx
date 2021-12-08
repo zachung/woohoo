b13ef078852bf21126f0970904086b2fb0d70de8
Brian <brian@hsress.com>
2021-12-08 23:58:10 +0800

    feat.build into iPhone

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import {
   GoogleSignin,
   GoogleSigninButton,
@@ -10,12 +10,12 @@ import {
 function App() {
   // Set an initializing state whilst Firebase connects
   const [user, setUser] = useState<User>({user:{}} as User);
+  const [msg, setMsg] = useState('');
 
   GoogleSignin.configure();
 
   function setState(param: { userInfo: User }) {
     setUser(param.userInfo)
-    console.log(param.userInfo)
   }
 
   // Somewhere in your code
@@ -26,13 +26,13 @@ function App() {
       setState({ userInfo });
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        // user cancelled the login flow
+        setMsg('user cancelled the login flow')
       } else if (error.code === statusCodes.IN_PROGRESS) {
-        // operation (e.g. sign in) is in progress already
+        setMsg('operation (e.g. sign in) is in progress already')
       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        // play services not available or outdated
+        setMsg('play services not available or outdated')
       } else {
-        // some other error happened
+        setMsg('some other error happened')
       }
     }
   };
@@ -48,19 +48,32 @@ function App() {
   }
   function UserInfo() {
     if (!user.idToken) {
-      return <></>
+      return <Text style={styles.innerText}>{msg}</Text>
     }
     return (
-        <Text>Welcome {user.user.email}</Text>
+        <Text style={styles.baseText}>Welcome {user.user.email}</Text>
     )
   }
 
+  const styles = StyleSheet.create({
+    mainView: {
+      backgroundColor: 'white',
+      flex: 1
+    },
+    baseText: {
+      fontWeight: 'bold'
+    },
+    innerText: {
+      color: 'red'
+    }
+  });
+
   return (
-    <View>
+    <View style={styles.mainView}>
       <Text />
       <Text />
       <Text />
-      <Text />
+      <Text>Hello Woohoo!</Text>
       <GoogleSignIn />
       <UserInfo />
     </View>
